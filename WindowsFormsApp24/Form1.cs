9acdf00275bc72afbfc7ab829eb35bff4b3e9aca
ko <korzhoff.i@gmail.com>
2019-05-23 15:20:39 +0300

    commit

--- WindowsFormsApp24/Form1.cs
+++ WindowsFormsApp24/Form1.cs
@@ -7,76 +7,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace WindowsFormsApp24
 {
     public partial class Form1 : Form
     {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            timer.Interval = (1000); 
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Invalidate(new Rectangle(10, 10, 300, 300));
+        }
         
-        int i = 20;
+
+        static Point RotatePoint(Point pointToRotate, Point centerPoint, double angleInDegrees)
+        {
+            double angleInRadians = angleInDegrees * (Math.PI / 180);
+            double cosTheta = Math.Cos(angleInRadians);
+            double sinTheta = Math.Sin(angleInRadians);
+            return new Point
+            {
+                X =
+                    (int)
+                    (cosTheta * (pointToRotate.X - centerPoint.X) -
+                    sinTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.X),
+                Y =
+                    (int)
+                    (sinTheta * (pointToRotate.X - centerPoint.X) +
+                    cosTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.Y)
+            };
+        }
+
+        Timer timer;
+        DateTime time;
+
+        int hours;
+        int minutes;
+        int seconds;
+
         public Form1()
         {
             InitializeComponent();
             this.Paint += Form1_Paint;
-            this.KeyDown += Form1_KeyDown;
             
+            timer = new Timer();
+            timer.Start();
+            timer.Tick += Timer_Tick;
+            this.Load += Form1_Load;
+
+
         }
-        Graphics g;
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            
+            time = DateTime.Now;
 
-            if (e.KeyCode == Keys.Down)
-            {
-                g = this.CreateGraphics();
-                i++;
-               
-            }
-            if (e.KeyCode == Keys.Up)
-            {
-                g = this.CreateGraphics();
-                i--;
-            }
-            if (e.KeyCode == Keys.Down)
-            {
-                g = this.CreateGraphics();
-                i++;
-
-            }
-            if (e.KeyCode == Keys.Up)
-            {
-                g = this.CreateGraphics();
-                i--;
-            }
-
-            Invalidate(new Rectangle(10, 10, 300, 300));
-            label1.Text = i.ToString();
-           
+            hours = time.Hour;
+            minutes = time.Minute;
+            seconds = time.Second;
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = this.CreateGraphics();
 
-            Pen pen = new Pen(Color.DimGray,10);
-            g.DrawEllipse(pen, 10, 10, 300, 300);
-            g.DrawLine(new Pen(Color.DarkViolet, 10), new Point(160, 170), new Point(160, 22));
+            Pen penForClock = new Pen(Color.DimGray, 10);
+            Pen penForMinute = new Pen(Color.DarkViolet, 10);
+            Pen penForHour = new Pen(Color.DarkGreen, 10);
+            Pen penForSecond = new Pen(Color.DarkOrange, 5);
 
+            penForHour.EndCap = LineCap.ArrowAnchor;
+            penForMinute.EndCap = LineCap.ArrowAnchor;
 
+            g.DrawEllipse(penForClock, 10, 10, 300, 300);
 
-            PointF PitCenter = new Point(picBoxZoomMap.Width / 2, picBoxZoomMap.Height / 2);
-            PointF p = new PointF(PitCenter.X - 20, PitCenter.Y - 250);
-            PointF p2 = new PointF(PitCenter.X + 20, PitCenter.Y - 250);
+            Point newPointMinutes = RotatePoint(new Point(160, 22), new Point(160, 160), minutes*6);
+            Point newPointHours = RotatePoint(new Point(160, 22), new Point(160, 160), hours * 30);
+            Point newPointSeconds = RotatePoint(new Point(160, 22), new Point(160, 160), seconds * 6);
 
-            var AlphaRad = RotDegrees * Math.PI / 180;
-
-            zoomgfx.DrawLine(Pens.Red, PitCenter, new PointF(
-            (float)(Math.Cos(AlphaRad) * (p.X - PitCenter.X) - Math.Sin(AlphaRad) * (p.Y - PitCenter.Y) + PitCenter.X),
-            (float)(Math.Sin(AlphaRad) * (p.X - PitCenter.X) + Math.Cos(AlphaRad) * (p.Y - PitCenter.Y) + PitCenter.Y)));
-
-            zoomgfx.DrawLine(Pens.Red, PitCenter, new PointF(
-            (float)(Math.Cos(AlphaRad) * (p2.X - PitCenter.X) - Math.Sin(AlphaRad) * (p2.Y - PitCenter.Y) + PitCenter.X),
-            (float)(Math.Sin(AlphaRad) * (p2.X - PitCenter.X) + Math.Cos(AlphaRad) * (p2.Y - PitCenter.Y) + PitCenter.Y)));
+            g.DrawLine(penForMinute, new Point(160, 160), newPointMinutes);
+            g.DrawLine(penForHour, new Point(160, 160), newPointHours);
+            g.DrawLine(penForSecond, new Point(160, 160), newPointSeconds);
 
 
 
